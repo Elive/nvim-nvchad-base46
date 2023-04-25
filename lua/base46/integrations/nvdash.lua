e00fd131c496374c2cacba563e4f2f3068100ff8
Thanatermesis <thanatermesis@gmail.com>
2023-04-25 16:58:20 -0500

    dashboard better bg color

--- lua/base46/integrations/nvdash.lua
+++ lua/base46/integrations/nvdash.lua
@@ -3,7 +3,7 @@ local colors = require("base46").get_theme_tb "base_30"
 return {
   NvDashAscii = {
     fg = colors.black,
-    bg = colors.blue,
+    bg = colors.dark_purple,
   },
 
   NvDashButtons = {
