947eb107489de39c3391ad3a263a6e4579d3ce3e
Thanatermesis <thanatermesis@gmail.com>
2023-04-26 22:08:34 -0500

    THEME MOLOKAI many improvements

--- lua/base46/themes/elive_molokai.lua
+++ lua/base46/themes/elive_molokai.lua
@@ -8,18 +8,45 @@
 
 local M = {}
 
+
+--  Colours base00 to base07 are typically variations of a shade and run from darkest to lightest. These colours are used for foreground and background, status bars, line highlighting and such. colours base08 to base0F are typically individual colours used for types, operators, names and variables. In order to create a dark theme, colours base00 to base07 should span from dark to light. For a light theme, these colours should span from light to dark.
+
+
 M.base_30 = {
   white = "#f5f4f1",
-  darker_black = "#22231D",
-  black = "#272822", --  nvim bg + tab selected
-  black2 = "#2F302A",  -- tab top line
-  one_bg = "#363731", -- real bg of onedark
-  one_bg2 = "#3E3F39",  -- bg hilighted for "selected block"
-  one_bg3 = "#464741",
+  -- black = "#272822", --  nvim bg + tab selected
+  -- black = "#1b1d1e", --  nvim bg + tab selected - same
+  black = M.base_16.base00, --  nvim bg + tab selected
+  -- darker_black = "#1b1f27", --   - 6%+ darker than black
+  -- darker_black = "#17121e", --   - 6%+ darker than black
+  darker_black = "#191b1c", --   - 6%+ darker than black
+  -- black2 = "#252931",  -- tab top line - 6%+ lighter than black
+  -- black2 = "#251d31",  -- tab top line - 6%+ lighter than black
+  black2 = "#282b2c",  -- tab top line - 6%+ lighter than black
+
+  -- one_bg = "#363731", --   - 10%+ lighter than black
+  -- one_bg = "#2e243d", --   - 10%+ lighter than black
+  one_bg = "#303436", --   - 10%+ lighter than black
+  -- one_bg2 = "#3E3F39",  -- bg hilighted for "selected block" - 19%+ lighter than black
+  -- one_bg2 = "#413357",  -- bg hilighted for "selected block" - 19%+ lighter than black
+  one_bg2 = "#44494b",  -- bg hilighted for "selected block" - 19%+ lighter than black
+  -- one_bg3 = "#464741",  --   - 27% lighter than black
+  -- one_bg3 = "#52416e",  --   - 27% lighter than black
+  one_bg3 = "#555b5e",  --   - 27% lighter than black
+
   grey = "#465457",   -- linenumbers - same
+  -- grey = "#6e5793",   -- linenumbers - 40% lighter than black (the % here depends so choose the perfect grey!)
+  -- grey = "#71797d",   -- linenumbers - 40% lighter than black (the % here depends so choose the perfect grey!)
   grey_fg = "#7e8e91",  -- comments - same
-  grey_fg2 = "#5D5E58",
-  light_grey = "#64655F",
+  -- grey_fg = "#7b63a3",  -- comments - 10% lighter than grey
+  -- grey_fg = "#56676b",  -- comments - 10% lighter than grey
+  -- grey_fg2 = "#5D5E58",
+  -- grey_fg2 = "#8a74ad",  --   - 20% lighter than grey
+  grey_fg2 = "#657a7e",  --   - 20% lighter than grey
+  -- light_grey = "#64655F",  --  -
+  -- light_grey = "#9682b5",  --  - 28% lighter than grey
+  light_grey = "#72898e",  --  - 28% lighter than grey
+
   -- red = "#e36d76",  -- orig
   red = "#f92672",
   baby_pink = "#f98385",
@@ -27,7 +54,8 @@ M.base_30 = {
   line = "#363942", -- for lines like vertsplit
   -- green = "#96c367",
   green = "#a6e22e",
-  vibrant_green = "#99c366",
+  -- vibrant_green = "#99c366",
+  vibrant_green = "#0add08",
   nord_blue = "#81A1C1",
   blue = "#51afef",
   yellow = "#e6c181",
@@ -40,17 +68,17 @@ M.base_30 = {
   orange = "#fd971f",
   -- cyan = "#41afef",
   cyan = "#66d9ef",
-  statusline_bg = "#2F302A",  -- statusline background
-  lightbg = "#3E3F39",  -- selected statusline
+  statusline_bg = "#2F302A",  -- statusline background - 4% lighter than black
+  lightbg = "#3E3F39",  -- selected statusline - 13% lighter than statusline_bg
+  -- lightbg = "#4c4d43",  -- selected statusline - 13% lighter than statusline_bg
+  lightbg2 = "#3e4038",  -- selected statusline - 7% lighter than statusline_bg
   pmenu_bg = "#a9c366",  -- menu popup selection
   folder_bg = "#61afef",
 }
 
---  Colours base00 to base07 are typically variations of a shade and run from darkest to lightest. These colours are used for foreground and background, status bars, line highlighting and such. colours base08 to base0F are typically individual colours used for types, operators, names and variables. In order to create a dark theme, colours base00 to base07 should span from dark to light. For a light theme, these colours should span from light to dark.
-
 M.base_16 = {
   -- base00 = "#272822",
-  -- base00 = "#1b1d1e",  -- Default Background - done
+  -- base00 = "#1b1d1e",  -- Default Background - done - same
   -- base00 = "#1c1726",  -- Default Background - done - galactic-purple version
   base00 = "#181320",  -- Default Background - done - galactic-purple version
   base01 = "#383830",  -- Lighter Background (Used for status bars, line number and folding marks), bg for folded blocks - done
@@ -87,6 +115,8 @@ M.base_16 = {
 M.polish_hl = {
   ["@variable"] = { fg = M.base_30.orange },
   ["@operator"] = { fg = M.base_30.red },
+  ["Folded"] = { bg = "NONE" },
+  -- ["@diffNewFile"] = { fg = M.base_30.blue }, -- not working, needs to be in section "added" ?
   -- ["@punctuation.bracket"] = { fg = M.base_30.green },
   -- ["@boolean"] = { fg = M.base_16.base09 },
   -- ["@conditional"] = { fg = M.base_16.base0A },
